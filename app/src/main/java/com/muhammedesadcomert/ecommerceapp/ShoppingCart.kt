d2ddd703e3703de14b53589fb3db7005d2de5b27
Muhammed Esad Cömert <esadcmrt@gmail.com>
2021-12-18 21:18:33 +0300

    Add shopping cart

--- app/src/main/java/com/muhammedesadcomert/ecommerceapp/ShoppingCart.kt
+++ app/src/main/java/com/muhammedesadcomert/ecommerceapp/ShoppingCart.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.muhammedesadcomert.ecommerceapp.adapter.CartAdapter
 import com.muhammedesadcomert.ecommerceapp.databinding.FragmentShoppingCartBinding
+import com.muhammedesadcomert.ecommerceapp.model.Product
 
 class ShoppingCart : Fragment(R.layout.fragment_shopping_cart) {
 
     private lateinit var binding: FragmentShoppingCartBinding
+    private lateinit var cart: ArrayList<Product>
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +30,39 @@ class ShoppingCart : Fragment(R.layout.fragment_shopping_cart) {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentShoppingCartBinding.inflate(layoutInflater)
+        db = Firebase.firestore
+        auth = Firebase.auth
+        cart = ArrayList()
+
+        db.collection("Users").document(auth.uid.toString()).collection("Cart")
+            .orderBy("date", Query.Direction.DESCENDING)
+            .addSnapshotListener { value, error ->
+                if (error != null) {
+                    Toast.makeText(activity, error.localizedMessage, Toast.LENGTH_LONG).show()
+                } else if (value != null && !value.isEmpty) {
+
+                    cart.clear()
+
+                    for (document in value.documents) {
+                        val id = document.id
+                        db.collection("Products").document(id).get().addOnSuccessListener {
+                            val name = it["name"] as String
+                            val price = it["price"] as String
+                            val category = it["category"] as String
+                            val imageURL = it["imageURL"] as String
+                            val product = Product(id, name, price, category, imageURL)
+                            cart.add(product)
+                            binding.recyclerView.apply {
+                                adapter = CartAdapter(cart)
+                                layoutManager = GridLayoutManager(context, 1)
+                                setHasFixedSize(true)
+                            }
+                        }.addOnFailureListener {
+                            Toast.makeText(activity, it.localizedMessage, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
         return binding.root
     }
 }
---
+++ app/src/main/java/com/muhammedesadcomert/ecommerceapp/adapter/CartAdapter.kt
@@ -0,0 +1,58 @@
+package com.muhammedesadcomert.ecommerceapp.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.muhammedesadcomert.ecommerceapp.R
+import com.muhammedesadcomert.ecommerceapp.databinding.FragmentShoppingCartCardBinding
+import com.muhammedesadcomert.ecommerceapp.model.Product
+import com.squareup.picasso.Picasso
+
+class CartAdapter(private val products: ArrayList<Product>) :
+    RecyclerView.Adapter<CartAdapter.ViewHolder>() {
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+
+    class ViewHolder(val binding: FragmentShoppingCartCardBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+        val cartCardBinding = FragmentShoppingCartCardBinding.inflate(adapterLayout, parent, false)
+        return ViewHolder(cartCardBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val product = products[position]
+
+        holder.binding.apply {
+            productTitle.text = product.name
+            productPrice.text = product.price
+            Picasso.get().load(product.imageURL).into(holder.binding.productImage)
+        }
+
+        holder.binding.cartProductCard.setOnClickListener {
+            val bundle = bundleOf("productId" to product.id)
+            Navigation.findNavController(it)
+                .navigate(R.id.action_shopping_cart_to_productPage, bundle)
+        }
+
+        holder.binding.deleteFromCart.setOnClickListener {
+            db = Firebase.firestore
+            auth = Firebase.auth
+
+            db.collection("Users").document(auth.uid.toString()).collection("Cart")
+                .document(product.id).delete()
+        }
+    }
+
+    override fun getItemCount() = products.size
+}
